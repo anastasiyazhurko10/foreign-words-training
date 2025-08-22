f9d3dd7dfd00a145ef1f3f244a52a62bc394b7d0
anastasiyazhurko10 <anastasiya.belanovich10@mail.ru>
2025-08-22 17:10:10 +0300

    Создаем массив слов

---
+++ script.js
@@ -0,0 +1,7 @@
+const words = [
+  { foreign: "apple", translation: "яблоко", example: "I eat an apple every day." },
+  { foreign: "book", translation: "книга", example: "She is reading a book." },
+  { foreign: "sun", translation: "солнце", example: "The sun is shining." },
+  { foreign: "water", translation: "вода", example: "Drink more water." },
+  { foreign: "friend", translation: "друг", example: "He is my best friend." }
+];
