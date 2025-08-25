c9b68f2113ed5697bd22ae4460a44f65062ca68b
anastasiyazhurko10 <anastasiya.belanovich10@mail.ru>
2025-08-25 20:36:41 +0300

    Создаю функцию для перемешивания слов, используя метод Фишера-Йетса

--- script.js
+++ script.js
@@ -92,3 +92,10 @@ showWord(currentIndex);
 updateNavigationButtons();
 
 const shuffleWords = document.getElementById("shuffle-words"); 
+
+function makeWordsShuffled(words) {
+  for (let i = words.length - 1; i > 0; i--) {
+    const j = Math.floor (Math.random() * (i + 1));
+    [words[i], words[j]] = [words[j], words[i]];
+  }
+};
